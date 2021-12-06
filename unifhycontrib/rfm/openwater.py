cc95839df7cc2607ee7dc76daa7b3e8699211fbb
Thibault Hallouin <thibault.hallouin@ucdconnect.ie>
2021-12-06 11:22:56 +0100

    remove remaining reference to old name

--- unifhycontrib/rfm/openwater.py
+++ unifhycontrib/rfm/openwater.py
@@ -128,7 +128,7 @@ class OpenWaterComponent(unifhy.component.OpenWaterComponent):
             **kwargs
     ):
 
-        # /!\__RENAMING_CM4TWC__________________________________________
+        # /!\__RENAMING_________________________________________________
         dt = self.timedelta_in_seconds
         shape = self.spaceshape
 
@@ -205,7 +205,7 @@ class OpenWaterComponent(unifhy.component.OpenWaterComponent):
             **kwargs
     ):
 
-        # /!\__RENAMING_CM4TWC__________________________________________
+        # /!\__RENAMING_________________________________________________
         dt = self.timedelta_in_seconds
         area = self.spacedomain.cell_area
 

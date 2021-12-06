7cd52aed17a56e37a24566a58cdaf4f9e72a9cbe
Thibault Hallouin <thibault.hallouin@ucdconnect.ie>
2021-12-06 11:19:39 +0100

    add zenodo JSON

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_namespace_packages
+import json
 
 
 pkg_name = 'RFM'
@@ -10,6 +11,17 @@ with open("unifhycontrib/{}/version.py".format(pkg_name.lower()), 'r') as fv:
     exec(fv.read())
 
 
+def read_authors(filename):
+    authors = []
+    with open(filename, 'r') as fz:
+        meta = json.load(fz)
+        for author in meta['creators']:
+            name = author['name'].strip()
+            authors.append(name)
+
+    return ', '.join(authors)
+
+
 setup(
     name='unifhycontrib-{}'.format(pkg_name.lower()),
 
@@ -19,7 +31,7 @@ setup(
     long_description=long_desc,
     long_description_content_type="text/x-rst",
 
-    author='Thibault Hallouin',
+    author=read_authors('.zenodo.json'),
 
     project_urls={
         'Source Code':
